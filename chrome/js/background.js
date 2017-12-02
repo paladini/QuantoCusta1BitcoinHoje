cb6424dcc006e67a76909e0d39621e6347e5e155
João Gusmão <joao.gusmao@apiki.com>
2017-12-02 19:11:14 -0200

    Abbreviating btc price to avoid breaking

--- chrome/js/background.js
+++ chrome/js/background.js
@@ -2,7 +2,6 @@ var value = 0.0;
 var response = "";
 
 function readTicker() {
-	
 	// Consultando API da Foxbit
 	var xhr	= new XMLHttpRequest();
 	xhr.open("GET", "https://api.blinktrade.com/api/v1/BRL/ticker?crypto_currency=BTC");
@@ -16,7 +15,9 @@ function readTicker() {
 
 				// Preparando o texto a ser impresso no badge (sem casas decimais).
 				var amount = Number(resp["last"]).toFixed(0);
-				
+				console.log(amount);
+				amount = abbrNum(amount,1);
+				console.log(amount);
 				// Fazendo localização para moeda e padrão de números brasileiro
 				var cotacao = parseFloat(resp["last"].toPrecision()).toLocaleString('pt-BR');
 				
@@ -26,13 +27,15 @@ function readTicker() {
 				}
 
 				// Determina cor de fundo do "badge"
+				var color = "#CC0000";
 				if (amount>value){
-					chrome.browserAction.setBadgeBackgroundColor({color: "#00CC00"});
-				} else {
-					chrome.browserAction.setBadgeBackgroundColor({color: "#CC0000"});
+					color = "#00CC00"
 				}
+
+				chrome.browserAction.setBadgeBackgroundColor({color: color});
+
 				value = amount;
-				
+
 				// Setando texto no Badge
 				chrome.browserAction.setBadgeText({text: String(amount)});
 				chrome.browserAction.setTitle({title: "O preço de 1 bitcoin agora é R$ " + cotacao});
@@ -47,6 +50,42 @@ function onAlarm(alarm) {
 	if (alarm && alarm.name == 'refresh') {
 		readTicker();
 	}
+}
+
+function abbrNum(number, decPlaces) {
+	// 2 decimal places => 100, 3 => 1000, etc
+	decPlaces = Math.pow(10,decPlaces);
+
+	// Enumerate number abbreviations
+	var abbrev = [ "k", "m", "b", "t" ];
+
+	// Go through the array backwards, so we do the largest first
+	for (var i=abbrev.length-1; i>=0; i--) {
+
+		// Convert array index to "1000", "1000000", etc
+		var size = Math.pow(10,(i+1)*3);
+
+		// If the number is bigger or equal do the abbreviation
+		if(size <= number) {
+			// Here, we multiply by decPlaces, round, and then divide by decPlaces.
+			// This gives us nice rounding to a particular decimal place.
+			number = Math.round(number*decPlaces/size)/decPlaces;
+
+			// Handle special case where we round up to the next abbreviation
+			if((number == 1000) && (i < abbrev.length - 1)) {
+				number = 1;
+				i++;
+			}
+
+			number += abbrev[i];
+			break;
+		}
+	}
+
+	if( number.length > 4 )
+		number = number.slice(0, -1);
+
+	return number;
 }
 
 chrome.runtime.onMessage.addListener(
